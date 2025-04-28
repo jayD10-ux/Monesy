6d7f494318ffa10f6e1f47ab6e2d1935c6bfbe1a
jayD10-ux <jaydeep.pawar10@gmail.com>
2025-04-28 11:19:58 +0530

    Sign up with phrases

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import WelcomeScreen from "./Components/WelcomeScreen";
 import ExpenseTracker from "./Components/ExpenseTracker";
+import PassphraseSignup from "./Components/PassphraseSignup"; 
 
 function App() {
   return (
@@ -8,6 +9,8 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<WelcomeScreen />} />
+          <Route path="/signup" element={<PassphraseSignup />} />
+          {/* Placeholder for login route */}
           <Route path="/dashboard" element={<ExpenseTracker />} />
           {/* If any unknown route, redirect to Welcome */}
           <Route path="*" element={<Navigate to="/" />} />
